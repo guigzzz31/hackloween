916013c3ced7f2dbeb7ae0dd8d9e38f36c1b801b
Vi <dupre.virginie@gmail.com>
2019-10-30 21:15:03 +0100

    pulling the ApiPage

---
+++ hackloween19/src/components/Haha1.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Haha1() {
+  return(
+    <div>
+      
+    </div>
+  )
+}
+export default Haha1;
---
+++ hackloween19/src/components/MovieDisplay.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './MovieDisplay.css'
+
+function MovieDisplay({ movie }) {
+  return (
+    <div className="card">
+      <h1>{movie.title && movie.title.replace(/_/g," ")}</h1>
+      <h2>{movie.year}</h2>
+      <img width="100%" src={movie.posterUrl} alt={movie.title && movie.title.replace(/_/g," ")} />
+      <p>Directed by: {movie.director && movie.director.replace(/_/g," ")}</p>
+      <p>{movie.country && movie.country.replace(/_/g," ")}</p>
+      <a href="#">Another Link</a>
+    </div>
+  )
+}
+export default MovieDisplay;
