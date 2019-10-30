95963c1ec1b8a4518c5b547634d15ab7b5e050d4
Camille Coin <camillecoin.cc@gmail.com>
2019-10-30 17:06:37 +0100

    created HomePage.jsx

---
+++ hackloween19/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return(
+    <div className="footer">
+      <p>© Wild Code School, Octobre 2019.</p>
+    </div>
+  )
+}
+
+export default Footer;
