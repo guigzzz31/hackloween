9a37861618d007a5df2eac390f7c6161a2053862
Navaloniaina Rakotovao <tsiresy+1@gmail.com>
2019-10-30 19:40:03 +0100

    waiting for the movie's and gif's pages

--- hackloween19/src/components/BoxPage.jsx
+++ hackloween19/src/components/BoxPage.jsx
@@ -4,35 +4,61 @@ import { Link } from 'react-router-dom';
 
 class BoxPage extends React.Component {
     constructor() {
-        super()
+        super();
     };
 
-
+   
     render() {
         return (
             <div className= "buttonsToClick">
                 <div className="buttonRang1">
-                    <button className="button" type="button" > </button>
-                    <button className="button" type="button" > </button>
+                    <button className="button" type="button" /*onClick={handleClick1}*/>
+                    <link to="/movie"></link>
+                    </button>
+                    <button className="button" type="button" >
+                    <link to="/movie"></link>
+                    </button>
                 </div>
                 <div className="buttonRang2">
-                    <button className="button" type="button" > </button>
-                    <button className="button" type="button" > </button>
-                    <button className="button" type="button" > </button>
+                    <button className="button" type="button" >
+                    <link to="/movie"></link>
+                    </button>
+                    <button className="button" type="button" >
+                    <link to="/movie"></link>
+                    </button>
+                    <button className="button" type="button" >
+                    <link to="/movie"></link>
+                    </button>
                 </div>
                 <div className="buttonRang3">
-                    <button className="button" type="button" > </button>
-                    <button className="button" type="button" > </button>
-                    <button className="button" type="button" > </button>
-                    <button className="button" type="button" > </button>
+                    <button className="button" type="button" >
+                    <link to="/movie"></link>
+                    </button>
+                    <button className="button" type="button" >
+                    <link to="/movie"></link>
+                    </button>
+                    <button className="button" type="button" >
+                    <link to="/movie"></link>
+                    </button>
+                    <button className="button" type="button" >
+                    <link to="/movie"></link>
+                    </button>
                 </div>
                 <div className="buttonRang4">
-                    <button className="button" type="button" > </button>
-                    <button className="button" type="button" > </button>
-                    <button className="button" type="button" > </button>
+                    <button className="button" type="button" >
+                    <link to="/movie"></link>
+                    </button>
+                    <button className="button" type="button" >
+                    <link to="/movie"></link>
+                    </button>
+                    <button className="button" type="button" >
+                    <link to="/movie"></link>
+                    </button>
                 </div>
                 <div className="buttonRang5">
-                    <button className="button" type="button" > </button>
+                    <button className="button" type="button" >
+                    <link to="/movie"></link>
+                    </button>
                 </div>
             </div>
         );
