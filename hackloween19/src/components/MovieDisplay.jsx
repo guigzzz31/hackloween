0197832997176d725631fc8fd87e45b2ab5c4c3c
Guigzzz31 <guillaume.trouche.6@gmail.com>
2019-10-30 21:09:56 +0100

    replace done wesh

--- hackloween19/src/components/MovieDisplay.jsx
+++ hackloween19/src/components/MovieDisplay.jsx
@@ -3,19 +3,13 @@ import './MovieDisplay.css'
 
 function MovieDisplay({ movie }) {
   return (
-    <div className="container">
-      <div className="card">
-        <div>
-          <h1>{movie.title}</h1>
-          <h2>{movie.year}</h2>
-        </div>
-        <img width="100%" src={movie.posterUrl} alt={movie.title} />
-        <div>
-          <p>Directed by: {movie.director}</p>
-          <p>{movie.country}</p>
-          <a href="#">Another Link</a>
-        </div>
-      </div>
+    <div className="card">
+      <h1>{movie.title && movie.title.replace(/_/g, " ")}</h1>
+      <h2>{movie.year}</h2>
+      <img width="100%" src={movie.posterUrl} alt={movie.title && movie.title.replace(/_/g, " ")} />
+      <p>By {movie.director && movie.director.replace(/_/g, " ")}</p>
+      <p>{movie.country && movie.country.replace(/_/g, " ")}</p>
+      <a href="#">Another Link</a>
     </div>
   )
 }
--- hackloween19/src/index.jsx
+++ hackloween19/src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-import './index.css';
+// import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
