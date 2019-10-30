42746d26c17172ee506c9372571a2403adbeec7a
Vi <dupre.virginie@gmail.com>
2019-10-30 22:33:50 +0100

    routes uncommented

--- hackloween19/src/App.jsx
+++ hackloween19/src/App.jsx
@@ -9,8 +9,6 @@ import Haha3 from './components/Haha3';
 import Haha4 from './components/Haha4';
 import Haha5 from './components/Haha5';
 import Haha6 from './components/Haha6';
-import MovieDisplay from './components/MovieDisplay'
-
 import './App.css';
 
 function App() {
@@ -19,13 +17,13 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/pick" component={BoxPage} />
-        {/* <Route path="/movie" component={ApiPage} /> */}
+        <Route path="/movie" component={ApiPage} />
         <Route path="/oops" component={Haha1} />
-        {/* <Route path="/mybad" component={Haha2} />
+        <Route path="/mybad" component={Haha2} />
         <Route path="/areyoulost" component={Haha3} />
         <Route path="/yesyouare" component={Haha4} />
         <Route path="/behindyou" component={Haha5} />
-        <Route path="/hereyougo" component={Haha6} /> */}
+        <Route path="/hereyougo" component={Haha6} />
       </Switch>
     </div>
   );
