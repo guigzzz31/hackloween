6e68c1f60617aaf5f7f73c500b7174758cdb2b88
Guigzzz31 <guillaume.trouche.6@gmail.com>
2019-10-31 00:18:22 +0100

    style

--- hackloween19/src/components/Haha2.jsx
+++ hackloween19/src/components/Haha2.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import gifZombie from './images/gifZombie.gif'
-import './HomePage.css';
+import gifMeuf from './images/meuf.gif'
+import './Troll.css';
 import { Link } from 'react-router-dom';
 import Footer from './Footer'
 
 function HomePage() {
   return(
   <div className="homepage">
-    <img src={gifZombie} alt="zombie coming out of the earth"/>
+    <img className="gif" src={gifMeuf} alt="zombie coming out of the earth"/>
     <h1>Still here?</h1>
     <p>Are you lost or something ?</p>
-    <h1><Link to="/areyoulost">I'm telling you to click here</Link></h1>
+    <h1><Link to="/pick">I'm telling you to click here</Link></h1>
     <Footer />
   </div>
   )
