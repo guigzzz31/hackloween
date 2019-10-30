a0a5572b49b770534bf110454a8045a281e0d64b
Vi <dupre.virginie@gmail.com>
2019-10-30 22:32:55 +0100

    link added in MovieDisplay

--- hackloween19/src/components/MovieDisplay.jsx
+++ hackloween19/src/components/MovieDisplay.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './MovieDisplay.css'
+import { Link } from 'react-router-dom';
 
 function MovieDisplay({ movie }) {
   return (
@@ -9,7 +10,7 @@ function MovieDisplay({ movie }) {
       <img width="100%" src={movie.posterUrl} alt={movie.title && movie.title.replace(/_/g, " ")} />
       <p>By {movie.director && movie.director.replace(/_/g, " ")}</p>
       <p>{movie.country && movie.country.replace(/_/g, " ")}</p>
-      <a href="#">Another Link</a>
+      <Link to="/">Another Link</Link>
     </div>
   )
 }
