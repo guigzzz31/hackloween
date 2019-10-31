44c325b6065d05a760fb0fb9e87cefe01a4e9d1b
Guigzzz31 <guillaume.trouche.6@gmail.com>
2019-10-31 11:06:18 +0100

    yo

--- hackloween19/src/components/Haha3.jsx
+++ hackloween19/src/components/Haha3.jsx
@@ -3,10 +3,12 @@ import gifClown from './images/clown.gif'
 import './Troll.css';
 import { Link } from 'react-router-dom';
 import Footer from './Footer'
+import ReactAudioPlayer from 'react-audio-player';
 
 function HomePage() {
   return(
   <div className="homepage">
+    <ReactAudioPlayer src="./audio/2856.mp3" autoPlay controls/>
     <img className="gif" src={gifClown} alt="zombie coming out of the earth"/>
     <h1>Oops</h1>
     <p>It appears you were send to a wrong place.</p>
--- hackloween19/src/components/MovieDisplay.jsx
+++ hackloween19/src/components/MovieDisplay.jsx
@@ -8,7 +8,7 @@ function MovieDisplay({ movie }) {
       <div className="card">
         <h1>{movie.title && movie.title.replace(/_/g, " ")}</h1>
         <h2>{movie.year}</h2>
-        <img width="100%" src={movie.posterUrl} alt={movie.title && movie.title.replace(/_/g, " ")} />
+        <img height="480px" width="70%" src={movie.posterUrl} alt={movie.title && movie.title.replace(/_/g, " ")} />
         <p>By {movie.director && movie.director.replace(/_/g, " ")}</p>
         <p>{movie.country && movie.country.replace(/_/g, " ")}</p>
       </div>
