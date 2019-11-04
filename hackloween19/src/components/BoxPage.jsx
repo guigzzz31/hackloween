f1e64bc773c54ba6b07d2ee3e76127192db38417
Camille Coin <camillecoin.cc@gmail.com>
2019-11-04 08:42:37 +0100

    final version

--- hackloween19/src/components/BoxPage.jsx
+++ hackloween19/src/components/BoxPage.jsx
@@ -3,42 +3,35 @@ import './BoxPage.css';
 import { Link } from 'react-router-dom';
 
 
-class BoxPage extends React.Component {
-    constructor() {
-        super()
-    };
-
-
-    render() {
-        return (
-            <div className= "buttonsToClick">
-                <h1>Choose wisely...</h1>
-                <div className="buttonRang1">
-                  <Link to="/Oops"><button className="button no-top" type="button" >? </button></Link>
-                  <Link to="/movie"><button className="button no-top" type="button" >? </button></Link>
-                </div>
-                <div className="buttonRang2">
-                  <Link to="/mybad"><button className="button" type="button" >? </button></Link>
-                  <Link to="/movie"><button className="button" type="button" >? </button></Link>
-                  <Link to="/areyoulost"><button className="button" type="button" >? </button></Link>
-                </div>
-                <div className="buttonRang3">
-                  <Link to="/movie"><button className="button" type="button" >? </button></Link>
-                  <Link to="/movie"><button className="button" type="button" >? </button></Link>
-                  <Link to="/booh"><button className="button" type="button" >? </button></Link>
-                  <Link to="/movie"><button className="button" type="button" >? </button></Link>
-                </div>
-                <div className="buttonRang4">
-                  <Link to="/yesyouare"><button className="button" type="button" >? </button></Link>
-                  <Link to="/movie"><button className="button" type="button" >? </button></Link>
-                  <Link to="/behindyou"><button className="button" type="button" >? </button></Link>
-                </div>
-                <div className="buttonRang5">
-                  <Link to="/behindyou"><button className="button" type="button" >? </button></Link>
-                </div>
-            </div>
-        );
-    }
+function BoxPage() {
+  return (
+    <div className="buttonsToClick">
+      <h1>Choose wisely...</h1>
+      <div className="buttonRang1">
+        <Link to="/Oops"><button className="button no-top" type="button" >? </button></Link>
+        <Link to="/movie"><button className="button no-top" type="button" >? </button></Link>
+      </div>
+      <div className="buttonRang2">
+        <Link to="/mybad"><button className="button" type="button" >? </button></Link>
+        <Link to="/movie"><button className="button" type="button" >? </button></Link>
+        <Link to="/areyoulost"><button className="button" type="button" >? </button></Link>
+      </div>
+      <div className="buttonRang3">
+        <Link to="/movie"><button className="button" type="button" >? </button></Link>
+        <Link to="/movie"><button className="button" type="button" >? </button></Link>
+        <Link to="/booh"><button className="button" type="button" >? </button></Link>
+        <Link to="/movie"><button className="button" type="button" >? </button></Link>
+      </div>
+      <div className="buttonRang4">
+        <Link to="/yesyouare"><button className="button" type="button" >? </button></Link>
+        <Link to="/movie"><button className="button" type="button" >? </button></Link>
+        <Link to="/movie"><button className="button" type="button" >? </button></Link>
+      </div>
+      <div className="buttonRang5">
+        <Link to="/behindyou"><button className="button" type="button" >? </button></Link>
+      </div>
+    </div>
+  );
 }
 
 
--- hackloween19/src/components/Haha3.jsx
+++ hackloween19/src/components/Haha3.jsx
@@ -1,21 +1,17 @@
 import React from 'react';
 import gifClown from './images/clown.gif'
 import './Troll.css';
-import { Link } from 'react-router-dom';
-import Footer from './Footer'
-import AudioPlayer from "react-h5-audio-player";
+import { NavLink } from 'react-router-dom';
 
-function HomePage() {
+function Haha3() {
   return(
-  <div className="homepage">
-    <AudioPlayer src="./audio/2856.mp3" autoPlay controls/>
+  <div className="troll-page">
     <img className="gif" src={gifClown} alt="zombie coming out of the earth"/>
     <h1>Oops</h1>
     <p>It appears you were send to a wrong place.</p>
-    <h1><Link to="/pick">Try again</Link></h1>
-    <Footer />
+    <h1><NavLink to="/pick" className="linkCss">Try again</NavLink></h1>
   </div>
   )
 }
 
-export default HomePage;
+export default Haha3;
